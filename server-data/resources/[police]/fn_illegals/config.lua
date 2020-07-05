c4c258d2814370cada45d26743ec2a175f23fdb6
SubhamPRO <prosubhamdas@yahoo.com>
2020-07-05 18:50:52 +0530

    push !

---
+++ server-data/resources/[police]/fn_illegals/config.lua
@@ -0,0 +1,16 @@
+Config = {}
+ 
+Config.MarkerColor = {r = 0, g = 200, b = 204}
+Config.JobName = "police" -- required  job
+Config.Bonus = 25 -- money bonus
+
+Config.CountMoney = 75
+Config.BonusMoney = 25
+
+Config.Items = {"cannabis", "coke", "meth", "coca_leaf", "opium"} -- List of the illegal items (do not add weapons!)
+
+Config.Zones = { 
+    {type = 22, x = 450.71, y = -910.69, z = 24.92} -- script zones
+}
+
+Config.Locale = 'en'
